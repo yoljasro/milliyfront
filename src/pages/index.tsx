b778870e3d98028f57f684abdc2eff83ed4fd8d8
yoljasro <saidaliyevjasur450@gmail.com>
2024-09-06 13:26:10 +0300

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
